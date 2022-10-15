09dea63b317746ca1c71b5cf19d58e69165a0823
Israel Pontes <israel.pontes.1920@gmail.com>
2022-10-15 19:26:22 -0300

    CrossOrigin

--- src/main/java/com/ms/email/controller/EmailController.java
+++ src/main/java/com/ms/email/controller/EmailController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/emails")
 public class EmailController {
